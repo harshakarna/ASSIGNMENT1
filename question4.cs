b18fb4c4f741f020d8da5ebd56e702616bdc69c7
harshakarna <84739619+harshakarna@users.noreply.github.com>
2021-06-03 23:52:02 -0400

    Update question4.cs

--- question4.cs
+++ question4.cs
@@ -1,83 +1,47 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace DIS_Summer_2021
 {
-   public static void Main(string[] args)
+	public class Program
+	{
+		public static void Main(string[] args)
 		{
-			Console.WriteLine(" Q4 : ");
-			int[] arr = { 1, 7, 3, 6, 5, 6 };
-			int pivot = PivotIndex(arr);
-			if (pivot > 0)
-			{
-				Console.WriteLine("The Pivot index for the given array is {0}", pivot);
-			}
-			else
-			{
-				Console.WriteLine("The given array has no Pivot index");
-			}
+			Console.WriteLine("Q6: Enter the sentence to convert:");
+			string sentence = Console.ReadLine();
+			string goatLatin = ToGoatLatin(sentence);
+			Console.WriteLine("Goat Latin for the Given Sentence is {0}", goatLatin);
+			Console.WriteLine();
 		}
-		public static int PivotIndex(int[] arr)
+		public static string ToGoatLatin(string sentence)
 		{
 			try
 			{
-				using System;
-				using System.Collections.Generic;
-				using System.Linq;
+				
+					StringBuilder goatlatin = new StringBuilder();
+					string[] sen_array = sentence.Split(" ");
+					int index = 1;
 
-namespace Assignment1_Summer2021
-	{
-		public class Program
-		{
-			public static void Main(string[] args)
-			{
-				Console.WriteLine(" Q4 : ");
+					foreach (string str in sen_array)
+					{
+						if ("aeiou".Contains(char.ToLower(str[0])))
+							goatlatin.Append(str + "ma" + string.Concat(Enumerable.Repeat("a", index)).ToString() + " ");
+						else if (!str.Contains("aeiouAEIOU"))
+							goatlatin.Append(str.Substring(1, str.Length - 1) + str.Substring(0, 1) + "ma" + string.Concat(Enumerable.Repeat("a", index)) + " ");
+						index++;
+					}
 
-				int[] arr = { 1, 7, 3, 6, 5, 6 };
-				int pivot = PivotIndex(arr);
-				if (pivot > 0)
-				{
-					Console.WriteLine("The Pivot index for the given array is {0}", pivot);
-				}
-				else
-				{
-					Console.WriteLine("The given array has no Pivot index");
-				}
+					goatlatin.Length--;
+					return goatlatin.ToString();
+
 			}
-			public static int PivotIndex(int[] nums)
+
+			catch (Exception e)
 			{
-				try
-				{
-					int add = 0;
-					foreach (int i in nums)
-					{
-						add = add + i;
-					}
-
-					int leftadd = 0;
-
-
-					for (int j = 0; j < nums.Length; j++)
-					{
-						if (leftadd == (add - leftadd - nums[j]))
-						{
-							return j;
-						}
-						leftadd = leftadd + nums[j];
-
-					}
-
-					return -1;
-
-				}
-
-				catch (Exception e)
-				{
-					throw;
-				}
+				throw;
 			}
 		}
 	}
-
 }
