570ea17a47ac8062f9e73f28c01b893d2e4f1f61
harshakarna <84739619+harshakarna@users.noreply.github.com>
2021-06-03 23:19:46 -0400

    Update question3.cs

--- question3.cs
+++ question3.cs
@@ -1,10 +1,45 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
-using System.Text;
+using System.Linq;
 
 namespace DIS_Summer_2021
 {
-    class question3
+    public class Program
     {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(" Q3 : ");
+            int[] arr = { 1, 1, 1, 1 };
+            int gp = NumIdenticalPairs(arr);
+            Console.WriteLine("The number of IdenticalPairs in the array are {0}:", gp);
+        }
+        public static int NumIdenticalPairs(int[] nums)
+        {
+            try
+            {
+                int pairs = 0;
+                Console.WriteLine(nums.Length);
+                for (int i = 0; i < nums.Length; i++)
+                {
+                    for (int j = i + 1; j < nums.Length; j++)
+                    {
+                        if (i < j)
+                        {
+                            if (nums[i] == nums[j])
+                            {
+
+                                pairs++;
+                            }
+                        }
+                    }
+                }
+                return pairs;
+            }
+
+            catch (Exception e)
+            {
+                throw;
+            }
+        }
     }
 }
