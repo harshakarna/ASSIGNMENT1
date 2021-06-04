3abb0be95f170cd299322cb280b7bc15cb4c1501
harshakarna <84739619+harshakarna@users.noreply.github.com>
2021-06-03 23:18:59 -0400

    Update question2.cs

--- question2.cs
+++ question2.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,5 +6,52 @@ namespace DIS_Summer_2021
 {
     class question2
     {
-    }
+        
+		public static void Main(string[] args)
+		{
+			Console.WriteLine(" Q2 : Enter the string to check for pangram:");
+			string s = Console.ReadLine();
+			s = s.ToLower();//Converting all the letters to lowercase
+			bool flag = CheckIfPangram(s);
+			if (flag)
+			{
+				Console.WriteLine("Yes, the given string is a pangram");
+			}
+			else
+			{
+				Console.WriteLine("No, the given string is not a pangram");
+			}
+		}
+		public static bool CheckIfPangram(String s)
+		{
+			try
+			{
+				int count = 0;
+				String all_eng_alp = "abcdefghijklmnopqrstuvwxyz";
+				String s_modified = new String(s.Distinct().ToArray());
+				for (int i = 0; i < all_eng_alp.Length; i++)
+				{
+					for (int j = 0; j < s_modified.Length; j++)
+					{
+						if (all_eng_alp[i] == s_modified[j]) 
+						{
+							count++;
+						}
+					}
+				}
+				Console.WriteLine(count);
+				if (count == 26) 				{
+					return true;
+				}
+				return false;
+			}
+
+			catch (Exception e)
+			{
+				throw;
+			}
+		}
+	}
 }
+    
+
